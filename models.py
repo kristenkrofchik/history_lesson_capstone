7a898f47d9f4d26102a9995550742be337340078
Kristen Krofchik <k.krofchik@gmail.com>
2021-02-19 17:45:09 -0600

    initial models.py

--- models.py
+++ models.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 bcrypt = Bcrypt()
 db = SQLAlchemy()
 
+
 class Follows(db.Model):
     """Followers and Following relationships"""
 
@@ -14,6 +15,7 @@ class Follows(db.Model):
     user_being_followed_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete="cascade"), primary_key=True)
 
     user_following_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete="cascade"), primary_key=True)
+
 
 class User(db.Model):
     """User/Teacher Model"""
@@ -48,7 +50,50 @@ class User(db.Model):
         secondaryjoin=(Follows.user_following_id == id)
     )
 
-    lessons = db.relationship('Lesson')
+    #lessons = db.relationship('Lesson')
+
+    @classmethod
+    def signup(cls, username, password, email, first_name, last_name, school, grade, location):
+        """Signs up user for profile.
+
+        Hashes password and adds user to database.
+        """
+
+        hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
+
+        user = User(
+            username=username,
+            password=hashed_pwd,
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+            school=school,
+            grade=grade,
+            location=location
+        )
+
+        db.session.add(user)
+        return user
+
+    
+    @classmethod
+    def authenticate(cls, username, password):
+        """Finds user with given username and password.
+
+        Searches for a user whose password hash matches this password
+        and, if it finds such a user, returns that user. If it doesn't 
+        find matching user (or if the password is incorrect), it will return False.
+        """
+
+        user = cls.query.filter_by(username=username).first()
+
+        if user:
+            is_auth = bcrypt.check_password_hash(user.password, password)
+            if is_auth:
+                return user
+
+        return False
+
 
 class Lesson(db.Model):
     """Lesson (Lesson Plan) Model"""
@@ -67,9 +112,10 @@ class Lesson(db.Model):
 
     user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
 
-    user = db.relationship('User', secondary='users_lessons', backref=lessons)
+    user = db.relationship('User', secondary='users_lessons', backref='lessons')
 
-    resources = db.relationship('Resource')
+    #resources = db.relationship('Resource')
+
 
 class UserLesson(db.Model):
     """Ties Between a User (Teacher) and their Lessons"""
@@ -79,4 +125,41 @@ class UserLesson(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
     
     lesson_id = db.Column(db.Integer, db.ForeignKey('lessons.id'), primary_key=True)
+
+
+class Resource(db.Model):
+    """Model for resource from LOC API"""
+
+    __tablename__ = "resources"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    title = db.Column(db.Text, nullable=False)
+
+    description = db.Column(db.Text)
+
+    loc_control_number = db.Column(db.Integer, nullable=False)
+
+    url = db.Column(db.Text, nullable=False)
+
+    lesson = db.relationship('Lesson', secondary='lessons_resources', backref='resources')
+
+
+class LessonResource(db.Model):
+    """Ties Between a lesson and its resources"""
+
+    __tablename__ = "lessons_resources"
+
+    lesson_id = db.Column(db.Integer, db.ForeignKey('lessons.id'), primary_key=True)
+    
+    resource_id = db.Column(db.Integer, db.ForeignKey('resources.id'), primary_key=True)
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+
 
