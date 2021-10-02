3dd862d131817be6cee760f617e2b4af1c98c25c
Kristen Krofchik <k.krofchik@gmail.com>
2021-10-02 09:41:31 -0500

    custom error pages

--- app.py
+++ app.py
@@ -582,6 +582,16 @@ def add_resource():
 
         return redirect(f"users/resources.html", user=user)
 
+"""Common Error Handling"""
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('errors/404.html'), 404
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('errors/500.html'), 500
+
 
 
 
