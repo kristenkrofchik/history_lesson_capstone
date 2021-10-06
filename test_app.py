ab62d266e1831d58cfb4596b2194b1cee90d5a8a
Kristen Krofchik <k.krofchik@gmail.com>
2021-10-06 02:17:48 -0500

    tests continue

--- test_app.py
+++ test_app.py
@@ -8,15 +8,23 @@ app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
 
 class HistoryLessonAppTestCase(TestCase):
     """Test routes"""
-    def test_main_page(self):
+    def test_main_page_redirection(self):
        with app.test_client() as client:
            resp = client.get('/')
-           html = resp.get_data(as_text=True)
 
-           self.assertEqual(resp.status_code, 200)
-           self.assertIn('<h1>Register/h1', html)
+           self.assertEqual(resp.status_code, 302)
+           self.assertEqual(resp.location, 'http://localhost/register')
     
-    def test_register(self):
+    def test_home_redirect_followed(self):
+        with app.test_client() as client:
+            resp = client.get('/', follow_redirects=True)
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<h1>Register</h1>', html)
+            
+    
+    def test_register_form(self):
         with app.test_client() as client:
             resp = client.post('/register',
                                 data={'username': 'test', 'password': 'testtest', 'confirm': 'testtest', 'email': 'test@test.com'})
